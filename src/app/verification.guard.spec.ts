710015143c915311748120749dd6349ce48da95e
ranika <ranikamadurawe@gmail.com>
2019-03-30 15:49:34 +0530

    verification guard done

--- src/app/verification.guard.spec.ts
+++ src/app/verification.guard.spec.ts
@@ -1,30 +1,75 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async, inject, fakeAsync, tick } from '@angular/core/testing';
 
 import { VerificationGuard } from './verification.guard';
 import { of, Observable } from 'rxjs';
 import { AuthServiceService} from './auth-service.service';
-import { Router } from '@angular/router';
-
-const AuthserviceMock = {
+import {ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from './mock/mock.component';
+import { Location } from '@angular/common';
+import {  Router, RouterOutlet, RouterLinkWithHref} from '@angular/router';
+/*const AuthserviceMock = {
     user: of({ uid: 'ABC123' , email:'ranika@gmail.com' , displayName:'okay', photoURL:'/img.jpg', emailVerified:true})
-};
-
-class RouterStub {
-    navigate(url: String) { return url}
-}
-const routerstub = new RouterStub();
+};*/
 
 
 describe('VerificationGuard', () => {
-  beforeEach(() => {
+  let verificationGuard: VerificationGuard;
+  let authService : AuthServiceService;
+  let next: ActivatedRouteSnapshot;
+  let state: RouterStateSnapshot;
+  let router : Router;
+  let location : Location;
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [VerificationGuard,
-      {provide : AuthServiceService, useValue : AuthserviceMock},
-      {provide : Router, useValue : routerstub}],
-    });
+      declarations: [  MockComponent ],
+      imports:[        AngularFireModule.initializeApp(environment.firebase),
+              AngularFireAuthModule,
+              AngularFirestoreModule,
+            RouterTestingModule.withRoutes([{ path: 'home', component : MockComponent },{ path: 'waiting-verification', component : MockComponent }])
+          ],
+      providers: [VerificationGuard, AuthServiceService],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    verificationGuard = TestBed.get(VerificationGuard);
+    authService = TestBed.get(AuthServiceService);
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
   });
 
-  it('should ...', inject([VerificationGuard], (guard: VerificationGuard) => {
+  it('should create', inject([VerificationGuard], (guard: VerificationGuard) => {
     expect(guard).toBeTruthy();
+  }));
+
+  it('be able to hit route when user is verified', fakeAsync(() => {
+    router.navigate(['/home']);
+    tick();
+    authService.user = of({ uid: 'ABC123' , email:'ranika@gmail.com' , displayName:'okay', photoURL:'/img.jpg', emailVerified:true});
+    verificationGuard.canActivate(next, state).subscribe(bool => {
+      expect(bool).toBe(true);
+      tick();
+      expect(location.path()).toBe('/home');
+    })
+
+  }));
+
+  it('not be able to hit route when user is not logged in', fakeAsync(() => {
+      router.navigate(['/home']);
+      tick();
+      authService.user = of({ uid: 'ABC123' , email:'ranika@gmail.com' , displayName:'okay', photoURL:'/img.jpg', emailVerified:false});
+      verificationGuard.canActivate(next, state).subscribe(bool => {
+        expect(bool).toBe(false);
+        tick();
+        expect(location.path()).toBe('/waiting-verification');
+      })
+
+
   }));
 });
--- src/app/waiting-verification/waiting-verification.page.spec.ts
+++ src/app/waiting-verification/waiting-verification.page.spec.ts
@@ -3,20 +3,32 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthServiceService } from '../auth-service.service';
 import { WaitingVerificationPage } from './waiting-verification.page';
 import { of, Observable } from 'rxjs';
-const AuthserviceMock = {
-    user: of({ uid: 'ABC123' , email:'ranika@gmail.com' , displayName:'okay', photoURL:'/img.jpg', emailVerified:true}),
-    signOut:()=>{this.user = null},
-    sendVerification:() => { console.log('vertification sent')}
-};
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('WaitingVerificationPage', () => {
   let component: WaitingVerificationPage;
   let fixture: ComponentFixture<WaitingVerificationPage>;
+  let authService : AuthServiceService;
+  let spy : jasmine.Spy;
+  let spy2 : jasmine.Spy;
+  let de : DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers:[{provide:AuthServiceService, userValue:AuthserviceMock}],
+      providers:[AuthServiceService],
       declarations: [ WaitingVerificationPage ],
+      imports :[
+          RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
@@ -26,9 +38,29 @@ describe('WaitingVerificationPage', () => {
     fixture = TestBed.createComponent(WaitingVerificationPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    de = fixture.debugElement;
+    authService = de.injector.get(AuthServiceService);
+    spy = spyOn(authService,'sendVerifciation').and.returnValue('sent verification mail');
+    spy2 = spyOn(authService,'signOut').and.returnValue('logged out');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should logout when clicking done', ()=>{
+    de.query(By.css('#donebutton')).nativeElement.dispatchEvent(new Event('click'));
+    expect(spy2).toHaveBeenCalled();
+  });
+
+  it('should logout when try with different', ()=>{
+
+      de.query(By.css('#trywithdiffbutton')).nativeElement.dispatchEvent(new Event('click'));
+      expect(spy2).toHaveBeenCalled();
+  });
+
+  it('should send verification when clicked',()=>{
+      de.query(By.css('#verbutton')).nativeElement.dispatchEvent(new Event('click'));
+      expect(spy).toHaveBeenCalled();
   });
 });
