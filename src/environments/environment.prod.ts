3fcfbf151910fec5fa3b5eaf9bd3c1ea4cb3a149
ranika <ranikamadurawe@gmail.com>
2019-05-22 22:16:19 +0530

    build environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyBOIp8dF6X8n3xm27EE0ULXSBEKhJHa4K8",
+    authDomain: "parkandgo-d6ee1.firebaseapp.com",
+    databaseURL: "https://parkandgo-d6ee1.firebaseio.com",
+    projectId: "parkandgo-d6ee1",
+    storageBucket: "parkandgo-d6ee1.appspot.com",
+    messagingSenderId: "320215829065"
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicmFuaWthbWFkdXJhd2UiLCJhIjoiY2pzcjRqdXNxMDh3azQ0cndmZW5qODhzaSJ9.J50qx45vzy9CYgQH5-Yv1w'
+  }
 };
