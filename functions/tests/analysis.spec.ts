3dad2758918253773a593b3d1318ea6629bac3a5
ranika <ranikamadurawe@gmail.com>
2019-05-29 15:15:30 +0530

    functions test

---
+++ functions/tests/analysis.spec.ts
@@ -0,0 +1,14 @@
+
+import 'mocha';
+// Assertion library.
+import {  expect } from 'chai';
+// Standalone test spies, stubs and mocks.
+
+
+describe('Analysis', () => {
+
+    it('should make the analysis', () => {
+        expect(2).to.equal(2);
+    });
+
+});
